3dad31f68f88a2d74ed10e553e4921f60824ca33
CHRISTIAN HILL <christian-hill-116@users.noreply.github.com>
2021-03-25 16:23:06 -0700

    Initial commit

---
+++ Dice Roller/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace Dice_Roller
+{
+    partial class DiceRoll
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiceRoll));
+            this.dice1 = new System.Windows.Forms.PictureBox();
+            this.dice6 = new System.Windows.Forms.PictureBox();
+            this.dice5 = new System.Windows.Forms.PictureBox();
+            this.dice4 = new System.Windows.Forms.PictureBox();
+            this.dice3 = new System.Windows.Forms.PictureBox();
+            this.dice2 = new System.Windows.Forms.PictureBox();
+            this.bigDice1 = new System.Windows.Forms.PictureBox();
+            this.bigDice2 = new System.Windows.Forms.PictureBox();
+            this.btn_roll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bigDice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bigDice2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dice1
+            // 
+            this.dice1.BackColor = System.Drawing.Color.Ivory;
+            this.dice1.Image = ((System.Drawing.Image)(resources.GetObject("dice1.Image")));
+            this.dice1.Location = new System.Drawing.Point(51, 12);
+            this.dice1.Name = "dice1";
+            this.dice1.Size = new System.Drawing.Size(100, 99);
+            this.dice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dice1.TabIndex = 0;
+            this.dice1.TabStop = false;
+            this.dice1.Visible = false;
+            this.dice1.Click += new System.EventHandler(this.dice1_Click);
+            // 
+            // dice6
+            // 
+            this.dice6.BackColor = System.Drawing.Color.Ivory;
+            this.dice6.Image = ((System.Drawing.Image)(resources.GetObject("dice6.Image")));
+            this.dice6.Location = new System.Drawing.Point(626, 12);
+            this.dice6.Name = "dice6";
+            this.dice6.Size = new System.Drawing.Size(100, 99);
+            this.dice6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dice6.TabIndex = 1;
+            this.dice6.TabStop = false;
+            this.dice6.Visible = false;
+            // 
+            // dice5
+            // 
+            this.dice5.BackColor = System.Drawing.Color.Ivory;
+            this.dice5.Image = ((System.Drawing.Image)(resources.GetObject("dice5.Image")));
+            this.dice5.Location = new System.Drawing.Point(511, 12);
+            this.dice5.Name = "dice5";
+            this.dice5.Size = new System.Drawing.Size(100, 99);
+            this.dice5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dice5.TabIndex = 2;
+            this.dice5.TabStop = false;
+            this.dice5.Visible = false;
+            this.dice5.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // dice4
+            // 
+            this.dice4.BackColor = System.Drawing.Color.Ivory;
+            this.dice4.Image = ((System.Drawing.Image)(resources.GetObject("dice4.Image")));
+            this.dice4.Location = new System.Drawing.Point(396, 12);
+            this.dice4.Name = "dice4";
+            this.dice4.Size = new System.Drawing.Size(100, 99);
+            this.dice4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dice4.TabIndex = 3;
+            this.dice4.TabStop = false;
+            this.dice4.Visible = false;
+            // 
+            // dice3
+            // 
+            this.dice3.BackColor = System.Drawing.Color.Ivory;
+            this.dice3.Image = ((System.Drawing.Image)(resources.GetObject("dice3.Image")));
+            this.dice3.Location = new System.Drawing.Point(281, 12);
+            this.dice3.Name = "dice3";
+            this.dice3.Size = new System.Drawing.Size(100, 99);
+            this.dice3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dice3.TabIndex = 4;
+            this.dice3.TabStop = false;
+            this.dice3.Visible = false;
+            // 
+            // dice2
+            // 
+            this.dice2.BackColor = System.Drawing.Color.Ivory;
+            this.dice2.Image = ((System.Drawing.Image)(resources.GetObject("dice2.Image")));
+            this.dice2.Location = new System.Drawing.Point(166, 12);
+            this.dice2.Name = "dice2";
+            this.dice2.Size = new System.Drawing.Size(100, 99);
+            this.dice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dice2.TabIndex = 5;
+            this.dice2.TabStop = false;
+            this.dice2.Visible = false;
+            // 
+            // bigDice1
+            // 
+            this.bigDice1.BackColor = System.Drawing.Color.Ivory;
+            this.bigDice1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bigDice1.Location = new System.Drawing.Point(304, 118);
+            this.bigDice1.Name = "bigDice1";
+            this.bigDice1.Size = new System.Drawing.Size(181, 178);
+            this.bigDice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bigDice1.TabIndex = 6;
+            this.bigDice1.TabStop = false;
+            // 
+            // bigDice2
+            // 
+            this.bigDice2.BackColor = System.Drawing.Color.Ivory;
+            this.bigDice2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.bigDice2.Location = new System.Drawing.Point(544, 118);
+            this.bigDice2.Name = "bigDice2";
+            this.bigDice2.Size = new System.Drawing.Size(181, 178);
+            this.bigDice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bigDice2.TabIndex = 7;
+            this.bigDice2.TabStop = false;
+            // 
+            // btn_roll
+            // 
+            this.btn_roll.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_roll.Location = new System.Drawing.Point(51, 117);
+            this.btn_roll.Name = "btn_roll";
+            this.btn_roll.Size = new System.Drawing.Size(194, 179);
+            this.btn_roll.TabIndex = 8;
+            this.btn_roll.Text = "Roll Dice";
+            this.btn_roll.UseVisualStyleBackColor = true;
+            this.btn_roll.Click += new System.EventHandler(this.btn_roll_Click);
+            this.btn_roll.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn_roll_MouseClick);
+            // 
+            // DiceRoll
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_roll);
+            this.Controls.Add(this.bigDice2);
+            this.Controls.Add(this.bigDice1);
+            this.Controls.Add(this.dice2);
+            this.Controls.Add(this.dice3);
+            this.Controls.Add(this.dice4);
+            this.Controls.Add(this.dice5);
+            this.Controls.Add(this.dice6);
+            this.Controls.Add(this.dice1);
+            this.Name = "DiceRoll";
+            this.Text = "Dice Roller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bigDice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bigDice2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox dice1;
+        private System.Windows.Forms.PictureBox dice6;
+        private System.Windows.Forms.PictureBox dice5;
+        private System.Windows.Forms.PictureBox dice4;
+        private System.Windows.Forms.PictureBox dice3;
+        private System.Windows.Forms.PictureBox dice2;
+        private System.Windows.Forms.PictureBox bigDice1;
+        private System.Windows.Forms.PictureBox bigDice2;
+        private System.Windows.Forms.Button btn_roll;
+    }
+}
+
---
+++ Dice Roller/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dice_Roller
+{
+    public partial class DiceRoll : Form
+    {
+        int counter = 0;
+        public DiceRoll()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dice1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_roll_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int box1 = new int();
+            int box2 = new int();
+
+            box1 = r.Next(0, 5);
+            box2 = r.Next(0, 5);
+
+            if (box1 == 0) bigDice1.Image = dice1.Image;
+            else if (box1 == 1) bigDice1.Image = dice2.Image;
+            else if (box1 == 2) bigDice1.Image = dice3.Image;
+            else if (box1 == 3) bigDice1.Image = dice4.Image;
+            else if (box1 == 4) bigDice1.Image = dice5.Image;
+            else bigDice1.Image = dice6.Image;
+
+            if (box2 == 0) bigDice2.Image = dice1.Image;
+            else if (box2 == 1) bigDice2.Image = dice2.Image;
+            else if (box2 == 2) bigDice2.Image = dice3.Image;
+            else if (box2 == 3) bigDice2.Image = dice4.Image;
+            else if (box2 == 4) bigDice2.Image = dice5.Image;
+            else bigDice2.Image = dice6.Image;
+
+            if (box1 == 0 && box2 == 0) MessageBox.Show("It took " + counter + " roll(s) to get snake eyes!" );
+        }
+
+        private void btn_roll_MouseClick(object sender, MouseEventArgs e)
+        {
+            counter++;
+        }
+    }
+}
